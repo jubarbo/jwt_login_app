848c60e5d73da211ef16ae1e84baf2225abb1251
Juan Barboza <jubarboza49@gmail.com>
2022-08-08 23:37:34 -0300

    use roles

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route, Routes } from 'react-router-dom';
 import { Register } from './components/Register';
 import { Login } from './components/Login';
 import Layout from './components/Layout';
+import Lounge from './components/Lounge';
 import Unauthorized from './components/Unauthorized';
 import Admin from './components/Admin';
 import Missing from './components/Missing';
@@ -10,6 +11,12 @@ import Editor from './components/Editor';
 import Home from './components/Home';
 import Links from './components/Links';
 import RequireAuth from './components/RequireAuth';
+
+const ROLES = {
+  'User': 2001,
+  'Editor': 1984,
+  'Admin': 5150
+} 
 
 function App() {
   return (
@@ -23,10 +30,20 @@ function App() {
         <Route path="unauthorized" element={<Unauthorized />} />
 
         {/* protected routes */}
-        <Route element={<RequireAuth />}>
+        <Route element={<RequireAuth allowedRoles={[ROLES.User]} />}>
           <Route path="/" element={<Home />} />
+        </Route>
+
+        <Route element={<RequireAuth allowedRoles={[ROLES.Editor]} />}>
           <Route path="editor" element={<Editor />} />
+        </Route>
+
+        <Route element={<RequireAuth allowedRoles={[ROLES.Admin]} />}>
           <Route path="admin" element={<Admin />} />
+        </Route>
+        
+        <Route element={<RequireAuth allowedRoles={[ROLES.Editor, ROLES.Admin]} />}>
+          <Route path="lounge" element={<Lounge />} />
         </Route>
 
         {/* catch routes */}
